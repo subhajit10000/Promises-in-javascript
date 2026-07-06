b06baaff3677454bb91a0f6be59789bcd60899cb
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-07-06 09:03:39 +0530

    Update Fetch_API.js

--- Fetch_API.js
+++ Fetch_API.js
@@ -1,1 +1,26 @@
 
+let response = fetch("https://api.restful-api.dev/objects/");
+console.log(response);
+
+response.then(function (data) {
+    console.log(data);
+    return data.json();
+}).then(function (myData) {
+    console.log(myData);
+
+}).catch(function () {
+    console.error('data fetching failed');
+
+})
+
+
+async function FetchData() {
+    let response = await fetch("https://api.restful-api.dev/objects/");
+    console.log(response);
+
+    let data = await response.json();
+    console.log(data);
+
+}
+
+FetchData();
